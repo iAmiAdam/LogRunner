c31a597e5f2ab929c0ef36671bf6eba704de56ef
Adam Smith <adam.smith18@ntlworld.com>
2014-06-28 16:13:42 +0100

    Functionality added roughly

--- core/src/info/adamjsmith/logrunner/LogRunner.java
+++ core/src/info/adamjsmith/logrunner/LogRunner.java
@@ -9,6 +9,15 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 
@@ -25,9 +34,19 @@ public class LogRunner extends ApplicationAdapter {
 	Array<Rectangle> logs;
 	SpriteBatch batch;
 	long lastLogTime;
+	World world;
+	Box2DDebugRenderer debugRenderer;
+	BodyDef playerDef;
+	Body playerBody;
+	Fixture fixture;
+	Rectangle rectangle;
+	Boolean landed;
 	
 	@Override
 	public void create () {
+		
+		world = new World(new Vector2(0, -10), true);
+		debugRenderer = new Box2DDebugRenderer();
 		
 		logImage = new Texture(Gdx.files.internal("log.png"));
 		riverImage = new Texture(Gdx.files.internal("river.png"));
@@ -58,11 +77,36 @@ public class LogRunner extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		
 		logs = new Array<Rectangle>();
+		log = new Rectangle();
+		log.x = 200;
+		log.y = 80;
+		log.width = 230;
+		log.height = 30;
+		logs.add(log);
 		spawnLog();
+		
+		playerDef = new BodyDef();
+		playerDef.type = BodyType.DynamicBody;
+		playerDef.position.set(390, 140);
+		
+		playerBody = world.createBody(playerDef);
+		
+		CircleShape circle = new CircleShape();
+		circle.setRadius(0.2f);
+		
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = circle;
+		fixtureDef.density = 1.0f;
+		fixtureDef.friction = 0.4f;
+		fixtureDef.restitution = 0.2f;
+		fixture = playerBody.createFixture(fixtureDef);
+		
+		landed = false;
+		
 	}
 	
 	private void spawnLog() {
-		Rectangle log = new Rectangle();
+		log = new Rectangle();
 		log.x = 800;
 		log.y = 80;
 		log.width = 230;
@@ -75,6 +119,8 @@ public class LogRunner extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(0.5f, 0.7f, 1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		Vector2 pos = playerBody.getPosition();
 		
 		camera.update();
 		
@@ -89,19 +135,31 @@ public class LogRunner extends ApplicationAdapter {
 		batch.draw(playerImage, player.x, player.y);
 		batch.end();
 		
-		if(Gdx.input.isTouched()) {
-			
-			
-		}
-		
-		if((TimeUtils.nanoTime() - lastLogTime) / 1000000000 > 1) spawnLog();
-		
 		Iterator<Rectangle> iter = logs.iterator();
 		while(iter.hasNext()) {
 			Rectangle log = iter.next();
 			log.x -= 250 * Gdx.graphics.getDeltaTime();
 			if(log.x + 230 == 0) iter.remove();
+			if(player.overlaps(log))
+				landed = true;
+			else
+				landed = false;
 		}
+		if(Gdx.input.isTouched() && landed == true ) {
+				playerBody.applyForceToCenter(0, 15f, true);
+		}	
+		
+		if (landed == true) {
+			player.y = 115;
+		} else {
+			pos = playerBody.getPosition();
+			player.y = pos.y;
+		}
+		
+		if((TimeUtils.nanoTime() - lastLogTime) / 1000000000 > 1) spawnLog();
+		
+		debugRenderer.render(world, camera.combined);
+		world.step(1/45f, 6, 2);
 	}
 	
 	@Override
