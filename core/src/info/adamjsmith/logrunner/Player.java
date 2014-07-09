c4b7e9b2191e18fc3c2c876f89b05931bf2b8af4
Adam Smith <adam.smith18@ntlworld.com>
2014-07-09 11:56:20 +0100

    Density independent Jumping finished

--- core/src/info/adamjsmith/logrunner/Player.java
+++ core/src/info/adamjsmith/logrunner/Player.java
@@ -77,7 +77,7 @@ public class Player extends GameObject {
 		if(this.playerState == PlayerState.LAND) {
 			game.assets.jump.play(0.1f);
 			pos = playerBody.getPosition();
-			float actualForce = ((-force / Gdx.graphics.getDensity()) / 1000) * 2;
+			float actualForce = ((-force / Gdx.graphics.getDensity()) / 1000) * 3f;
 			if (actualForce > 8) actualForce = 8;
 			playerBody.applyLinearImpulse(0, actualForce, pos.x, pos.y, true);
 			Gdx.app.log("Force", String.valueOf(actualForce));
